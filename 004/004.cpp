d7175f712651264741db35f7732052f798b00d70
dongha <donghak937@gmail.com>
2025-06-22 23:33:25 +0900

    add 003

---
+++ 004/004.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    const int CREATE_VICTORY_SALSU = 612;
+    const int CREATE_VICTORY_GWIJU = 1019;
+    
+
+    cout << "고구려 살수대첩 연도 : " << CREATE_VICTORY_SALSU << "년"<< endl;
+    cout << "고려 귀주년도 연도: " << CREATE_VICTORY_GWIJU << "년"<< endl;
+    
+    return 0;
+}

3f723c3d34319b8a688db05c5879adeeb0590193
dongha <donghak937@gmail.com>
2025-06-22 23:33:44 +0900

    add 004

--- 004/004.cpp
+++ 004/004.cpp
@@ -9,7 +9,7 @@ int main(){
     
 
     cout << "고구려 살수대첩 연도 : " << CREATE_VICTORY_SALSU << "년"<< endl;
-    cout << "고려 귀주년도 연도: " << CREATE_VICTORY_GWIJU << "년 "<< endl;
+    cout << "고려 귀주년도 연도: " << CREATE_VICTORY_GWIJU << "년"<< endl;
     
     return 0;
 }

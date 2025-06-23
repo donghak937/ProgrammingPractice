dcadc904aa9c4f3f560720020848bc2b37f155bf
dongha <donghak937@gmail.com>
2025-06-23 14:15:54 +0900

    add 034

--- 기초/034/034.cpp
+++ 기초/034/034.cpp
@@ -9,7 +9,7 @@ int main(){
     for(int i = 0; i < 10; i++){
         if (i % 3 == 0) continue;
         else if (i == number) break;
-        else cout << "햔제 i 값 : " << i << endl;
+        else cout << "현재 i 값 : " << i << endl;
     }
 
     return 0;

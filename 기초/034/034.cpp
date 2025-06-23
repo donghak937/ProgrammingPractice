b6ce17c8f7b3adde8cd4bbe3602dbaefcfa87897
dongha <donghak937@gmail.com>
2025-06-23 14:15:03 +0900

    add 034

---
+++ 기초/034/034.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int number = 7;
+
+    for(int i = 0; i < 10; i++){
+        if (i % 3 == 0) continue;
+        else if (i == number) break;
+        else cout << "햔제 i 값 : " << i << endl;
+    }
+
+    return 0;
+}

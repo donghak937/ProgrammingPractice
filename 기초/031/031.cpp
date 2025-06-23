476022e66445bb25d786f0aef69caf4853b46b40
dongha <donghak937@gmail.com>
2025-06-23 13:47:47 +0900

    add 031

---
+++ 기초/031/031.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <bitset>
+
+using namespace std;
+
+class Temp{
+    int no; // 4
+    bool is_on; // 1
+
+    // 2번 이동 (4bit 단위)
+
+};
+
+int main(){
+
+    cout << "char 크기 : " << sizeof('p') << endl;
+    cout << "int 크기 : " << sizeof(1) << endl;
+    cout << "double 크기 : " << sizeof(1.0) << endl;
+    cout << "클래스 크기 : " << sizeof(Temp) << endl;
+
+    return 0;
+}
